2ef994992b14c370b8d82a4e40db464a0e85a5c2
yuktadandeva <153160647+yuktadandeva@users.noreply.github.com>
2024-11-14 16:05:56 +0530

    Update Footer.jsx

--- src/Shared/Components/Footer/Footer.jsx
+++ src/Shared/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <div className='footer'>
       <ul className='flex'>
        
-        <li><a href="/public/utils/cv/yukta_dandeva.pdf" download="yukta_dandeva.pdf" target="_blank">CV </a></li>
+        <li><a href="/utils/cv/yukta_dandeva.pdf" download="yukta_dandeva.pdf" target="_blank">CV </a></li>
         <li><a href="https://www.linkedin.com/in/yukta-dandeva-121a03257/" target='/blank'>Linkedin</a></li>
         <li><a href="https://github.com/yuktadandeva" target='/blank'>Github</a></li>
         <li><a href="mailto:yuktadandeva@gmail.com" target='/blank'>Mail</a></li>
